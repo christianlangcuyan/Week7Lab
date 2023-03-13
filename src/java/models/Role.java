2a39e62c186c989befac6c716783e76ca7086664
Chris <Chris@10.0.0.161>
2023-03-12 18:25:18 -0600

    Added attributes for User and Role classes

--- src/java/models/Role.java
+++ src/java/models/Role.java
@@ -1,14 +1,37 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package models;
+
+import java.io.Serializable;
 
 /**
  *
  * @author Chris
  */
-public class Role {
+public class Role implements Serializable {
+    private int roleID;
+    private String roleName;
     
+    public Role(int roleID) {
+        this.roleID = roleID;
+        if (roleID == 1) {
+            roleName = "system admin";
+        } else {
+            roleName = "regular user";
+        }
+    }
+
+    public int getRoleID() {
+        return roleID;
+    }
+
+    public void setRoleID(int roleID) {
+        this.roleID = roleID;
+    }
+
+    public String getRoleName() {
+        return roleName;
+    }
+
+    public void setRoleName(String roleName) {
+        this.roleName = roleName;
+    }
 }
